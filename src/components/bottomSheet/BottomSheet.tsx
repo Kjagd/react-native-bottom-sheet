9797000a01d1e6ff21e65ebfc0122e05a3bbd669
Kristian Jagd <kristianjagd@gmail.com>
2022-06-08 12:35:06 +0200

    fix: modal re-apperance on Android

--- src/components/bottomSheet/BottomSheet.tsx
+++ src/components/bottomSheet/BottomSheet.tsx
@@ -1187,8 +1187,9 @@ const BottomSheetComponent = forwardRef<BottomSheet, BottomSheetProps>(
     //#region styles
     const containerAnimatedStyle = useAnimatedStyle(
       () => ({
-        opacity:
-          Platform.OS === 'android' && animatedIndex.value === -1 ? 0 : 1,
+        // Breaks modal re-appearances on Android
+        // opacity:
+        //   Platform.OS === 'android' && animatedIndex.value === -1 ? 0 : 1,
         transform: [
           {
             translateY: animatedPosition.value,
